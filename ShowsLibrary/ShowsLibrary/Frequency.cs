a3607af8bebd1f13cf3bcfb9ccfb873a4229e3cf
lmaleeva <113145975+lmaleeva@users.noreply.github.com>
2023-06-20 17:15:13 +0500

    v3.t12

---
+++ ShowsLibrary/ShowsLibrary/Frequency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowsLibrary
+{
+    public enum Frequency
+    {
+        Daily,
+        Weekly,
+        Monthly,
+        NonPeriodic
+    }
+}
---
+++ ShowsLibrary/ShowsLibrary/Show.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShowsLibrary;
+
+namespace ShowsLibrary
+{
+    public class Show
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Host { get; set; }
+        public readonly Frequency Frequency;
+        public readonly DateTime Time;
+        public Show(string title, string description, string host, Frequency frequency, string time)
+        {
+            Title = title;
+            Description = description;
+            Host = host;
+            Frequency = frequency;
+            Time = DateTime.Parse(time);
+        }
+        public string GetInfo()
+        {
+            var frequency = "";
+
+            if (Frequency == Frequency.NonPeriodic)
+            {
+                frequency = "Not periodic";
+                return $"{frequency} {Title} {Description}. Ведущий {Host}. Дата выхода: {Time:g}.";
+            }
+            else
+            {
+                frequency = Frequency.ToString(frequency);
+                return $"{frequency} {Title} {Description}. Ведущий {Host}. Дата выхода: {Time:d}.";
+            }
+
+        }
+    }
+}
